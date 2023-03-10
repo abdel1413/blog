b73a849b7f4f5cdf52c6cac32b53774487664beb
Your Name <you@example.com>
2023-03-10 11:26:51 -0500

    made a modifications on methods

--- Practice/select.js
+++ Practice/select.js
@@ -3,9 +3,20 @@ let select = document.querySelector("#framework");
 
 btn.onclick = (event) => {
   event.preventDefault();
+
   console.log(select.selectedIndex);
   console.log(select.value);
   alert(
     `the selected item value: ${select.value} with index ${select.selectedIndex}`
   );
 };
+
+//accessing select elt options collection
+let selectBox = select.options;
+
+//looping through
+Array.from(selectBox).forEach((e) => console.log(e.text));
+//accessing innerHtml and value
+let selectOption = select.options[select.selectedIndex];
+console.log("selectOption is: ", selectOption.text);
+console.log("and ist value is :", selectOption.value);
