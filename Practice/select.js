c7352f9da5d3fb5966df16211090e7a85a9986a0
Your Name <you@example.com>
2023-03-11 11:19:56 -0500

    debaguing

--- Practice/select.js
+++ Practice/select.js
@@ -1,4 +1,4 @@
-let btn = document.querySelector("#btn");
+let btn = document.querySelector(".btn");
 let select = document.querySelector("#framework");
 
 btn.onclick = (event) => {
@@ -16,7 +16,27 @@ let selectBox = select.options;
 
 //looping through
 Array.from(selectBox).forEach((e) => console.log(e.text));
+
 //accessing innerHtml and value
 let selectOption = select.options[select.selectedIndex];
 console.log("selectOption is: ", selectOption.text);
 console.log("and ist value is :", selectOption.value);
+
+//getting multiple elements
+let multSelect = document.querySelector(".multiple");
+let btn2 = document.querySelector(".btn2");
+
+console.log("mult", multSelect);
+for (let op of multSelect) {
+  console.log(op.text, op.value);
+}
+
+console.log(multSelect);
+
+btn2.onclick = (event) => {
+  event.preventDefault();
+  let multy = [].filter
+    .call(multSelect.options, (option) => option.selected)
+    .map((op) => op.text);
+  alert(multy);
+};
